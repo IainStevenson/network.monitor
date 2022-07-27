314e51358b226f4a1d4752ae12bbed8d16dce23a
Iain Stevenson <31848657+IainStevenson@users.noreply.github.com>
2022-07-27 21:52:25 +0100

    Cleanup

--- src/netmon.cli/Program.cs
+++ src/netmon.cli/Program.cs
@@ -7,167 +7,146 @@ using netmon.core.Orchestrators;
 using netmon.core.Storage;
 using System.Net;
 
-/// <summary>
-/// Inspired by : https://stackoverflow.com/questions/5891538/listen-for-key-press-in-net-console-app
-/// </summary>
-public static class Program
+namespace netmon.cli
 {
 
-    private static CancellationTokenSource cancellationTokenSource;
-    private static IMonitorOrchestrator monitorOrchestrator;
-    static void Main(string[] args)
+    /// <summary>
+    /// Inspired by : https://stackoverflow.com/questions/5891538/listen-for-key-press-in-net-console-app
+    /// </summary>
+    public static class Program
     {
-        var serviceProvider = new ServiceCollection()
-                   .AddLogging()
-                   .AddSingleton<PingHandlerOptions>() // the defaults are good here
-                   .AddSingleton<TraceRouteOrchestratorOptions>()// the defaults are good here
-                   .AddSingleton<PingOrchestratorOptions>() // the defaults are good here
-                   .AddSingleton<IPingRequestModelFactory, PingRequestModelFactory>()
-                   .AddTransient<IPingHandler, PingHandler>()
-                   .AddSingleton<ITraceRouteOrchestrator, TraceRouteOrchestrator>()
-                   .AddSingleton<IPingOrchestrator, PingOrchestrator>()
-                   .AddSingleton<IStorage<PingResponseModel>, PingResponseModelInMemoryStorage>()
-                   .AddSingleton<IMonitorOrchestrator, MonitorOrchestrator>()
-                   .AddSingleton<CancellationTokenSource>()
-                   .BuildServiceProvider();
 
-        cancellationTokenSource = serviceProvider.GetRequiredService<CancellationTokenSource>();
-        monitorOrchestrator = serviceProvider.GetRequiredService<IMonitorOrchestrator>();
+        private static CancellationTokenSource cancellationTokenSource;
+        private static IMonitorOrchestrator monitorOrchestrator;
+        static void Main(string[] args)
 
-        Console.CancelKeyPress += (sender, e) =>
         {
-            // cancel token
-            cancellationTokenSource.Cancel();   
-            Console.WriteLine("Exiting...");
-            Environment.Exit(0);
-        };
+            var serviceProvider = new ServiceCollection()
+                       .AddLogging()
+                       .AddSingleton<PingHandlerOptions>() // the defaults are good here
+                       .AddSingleton<TraceRouteOrchestratorOptions>()// the defaults are good here
+                       .AddSingleton<PingOrchestratorOptions>() // the defaults are good here
+                       .AddSingleton<IPingRequestModelFactory, PingRequestModelFactory>()
+                       .AddTransient<IPingHandler, PingHandler>()
+                       .AddSingleton<ITraceRouteOrchestrator, TraceRouteOrchestrator>()
+                       .AddSingleton<IPingOrchestrator, PingOrchestrator>()
+                       .AddSingleton<IStorage<PingResponseModel>, PingResponseModelInMemoryStorage>()
+                       .AddSingleton<IMonitorOrchestrator, MonitorOrchestrator>()
+                       .AddSingleton<CancellationTokenSource>()
+                       .BuildServiceProvider();
 
-        Console.WriteLine("Press ESC to Exit");
+            cancellationTokenSource = serviceProvider.GetRequiredService<CancellationTokenSource>();
+            monitorOrchestrator = serviceProvider.GetRequiredService<IMonitorOrchestrator>();
+
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                // cancel token
+                cancellationTokenSource.Cancel();
+                Console.WriteLine("Exiting...");
+                Environment.Exit(0);
+            };
+
+            Console.WriteLine("Press ESC to Exit");
 
 
-        var taskKeys = new Task(ReadKeys);
-        taskKeys.Start();
+            var taskKeys = new Task(ReadKeys);
+            taskKeys.Start();
 
-        //var taskProcessFiles = new Task(ProcessFiles);
-        //taskProcessFiles.Start();
+            //var taskProcessFiles = new Task(ProcessFiles);
+            //taskProcessFiles.Start();
 
-        var taskMonitor = new Task( async () => await monitorOrchestrator.Execute(new List<IPAddress>(),
-                                        new TimeSpan(99,23,59,59,999),
-                                        false,
-                                        cancellationTokenSource.Token));
+            var taskMonitor = new Task(async () => await monitorOrchestrator.Execute(new List<IPAddress>(),
+                                            new TimeSpan(99, 23, 59, 59, 999),
+                                            false,
+                                            cancellationTokenSource.Token));
 
-        taskMonitor.Start();
+            taskMonitor.Start();
 
-        var tasks = new[] { taskKeys };
-        Task.WaitAll(tasks);
-    }   
+            var taskBusy = new Task(BusyIndicator);
+            taskBusy.Start();
 
-    private static void ProcessFiles()
-    {
-        var files = Enumerable.Range(1, 100).Select(n => "File" + n + ".txt");
+            var tasks = new[] { taskMonitor ,taskKeys};
+            Task.WaitAll(tasks);
+        }
 
-        var taskBusy = new Task(BusyIndicator);
-        taskBusy.Start();
+       
 
-        foreach (var file in files)
+        private static void BusyIndicator()
         {
-            Thread.Sleep(1000);
-            Console.WriteLine("Procesing file {0}", file);
+            var busy = new ConsoleBusyIndicator();
+            busy.UpdateProgress();
+        }
+
+        private static void ReadKeys()
+        {
+            ConsoleKeyInfo key = new();
+
+            while (!Console.KeyAvailable && key.Key != ConsoleKey.Escape)
+            {
+
+                key = Console.ReadKey(true);
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Console.WriteLine("UpArrow was pressed");
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.WriteLine("DownArrow was pressed");
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        Console.WriteLine("RightArrow was pressed");
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        Console.WriteLine("LeftArrow was pressed");
+                        break;
+
+                    case ConsoleKey.Escape:
+                        cancellationTokenSource.Cancel();
+                        break;
+
+                    default:
+                        if (Console.CapsLock && Console.NumberLock)
+                        {
+                            Console.WriteLine(key.KeyChar);
+                        }
+                        break;
+                }
+            }
         }
     }
 
-    private static void BusyIndicator()
+    internal class ConsoleBusyIndicator
     {
-        var busy = new ConsoleBusyIndicator();
-        busy.UpdateProgress();
-    }
+        int _currentBusySymbol;
 
-    private static void ReadKeys()
-    {
-        ConsoleKeyInfo key = new ConsoleKeyInfo();
+        public char[] BusySymbols { get; set; }
 
-        while (!Console.KeyAvailable && key.Key != ConsoleKey.Escape)
+        public ConsoleBusyIndicator()
         {
-
-            key = Console.ReadKey(true);
-
-            switch (key.Key)
+            BusySymbols = new[] { '|', '/', '-', '\\' };
+        }
+        public void UpdateProgress()
+        {
+            while (true)
             {
-                case ConsoleKey.UpArrow:
-                    Console.WriteLine("UpArrow was pressed");
-                    break;
-                case ConsoleKey.DownArrow:
-                    Console.WriteLine("DownArrow was pressed");
-                    break;
+                Thread.Sleep(100);
+                var originalX = Console.CursorLeft;
+                var originalY = Console.CursorTop;
 
-                case ConsoleKey.RightArrow:
-                    Console.WriteLine("RightArrow was pressed");
-                    break;
+                Console.Write(BusySymbols[_currentBusySymbol]);
 
-                case ConsoleKey.LeftArrow:
-                    Console.WriteLine("LeftArrow was pressed");
-                    break;
+                _currentBusySymbol++;
 
-                case ConsoleKey.Escape:
-                    break;
+                if (_currentBusySymbol == BusySymbols.Length)
+                {
+                    _currentBusySymbol = 0;
+                }
 
-                default:
-                    if (Console.CapsLock && Console.NumberLock)
-                    {
-                        Console.WriteLine(key.KeyChar);
-                    }
-                    break;
+                Console.SetCursorPosition(originalX, originalY);
             }
         }
     }
 }
-
-internal class ConsoleBusyIndicator
-{
-    int _currentBusySymbol;
-
-    public char[] BusySymbols { get; set; }
-
-    public ConsoleBusyIndicator()
-    {
-        BusySymbols = new[] { '|', '/', '-', '\\' };
-    }
-    public void UpdateProgress()
-    {
-        while (true)
-        {
-            Thread.Sleep(100);
-            var originalX = Console.CursorLeft;
-            var originalY = Console.CursorTop;
-
-            Console.Write(BusySymbols[_currentBusySymbol]);
-
-            _currentBusySymbol++;
-
-            if (_currentBusySymbol == BusySymbols.Length)
-            {
-                _currentBusySymbol = 0;
-            }
-
-            Console.SetCursorPosition(originalX, originalY);
-        }
-    }
-}
-
-//// See https://aka.ms/new-console-template for more information
-
-
-//var monitorOrchestrator = serviceProvider.GetRequiredService<IMonitorOrchestrator>();
-
-//if (args.Length == 0)
-//{
-//   
-
-//    }
-
-
-
-//}
-
-
-//return 1;
